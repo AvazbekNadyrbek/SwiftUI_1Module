a0aadc632b6d61e2358ed49d80d745f89424989e
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2025-07-09 13:33:43 +0200

    Initial Commit

---
+++ SwiftUI_1Module/SwiftUI_1ModuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_1ModuleApp.swift
+//  SwiftUI_1Module
+//
+//  Created by Авазбек Надырбек уулу on 09.07.25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_1ModuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
